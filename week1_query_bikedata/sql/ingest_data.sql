e802ad76ca8cf277d57481dea597353e7ddb91ad
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2025-11-13 14:07:29 +0100

    uppdate

---
+++ week1_query_bikedata/sql/ingest_data.sql
@@ -0,0 +1,13 @@
+
+CREAT SCHEMA IF NOT EXISTS staging; 
+
+
+
+CREATE TABLE IF NOT EXISTS staging.orders AS (
+    SELECT * FROM read_csv_auto('data/orders.csv')
+    );
+
+
+    CREATE TABLE IF NOT EXISTS staging.customers AS (
+    SELECT * FROM read_csv_auto('data/customers.csv')
+    );
