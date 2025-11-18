3795f4c1919a543209c18ea843201dba8d449bde
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2025-11-18 12:27:38 +0100

    uppdate crud_hr.sql code

---
+++ week2_crud_aggregate_hrdata/sql/crud_hr.sql
@@ -0,0 +1,50 @@
+
+
+/* ============
+   DDL - CREATE
+   ============ */
+-- create schema
+CREATE SCHEMA IF NOT EXISTS staging; 
+
+-- create sequence to generate values for employee_id column later
+CREATE SEQUENCE IF NOT EXISTS id_sequence START 1; 
+
+-- create table
+CREATE TABLE IF NOT EXISTS staging.employees (
+    employee_id INTEGER DEFAULT nextval('id_sequence'),
+    department VARCHAR,
+    employment_year INTEGER
+); 
+
+/* =============
+   CRUD - CREATE 
+   ============= */
+-- insert 3 rows manually
+INSERT INTO
+    staging.employees (department, employment_year)
+VALUES
+    ('Sales', 2001),
+    ('Logistics', 2002),
+    ('IT', 2002);
+
+-- insert with read_csv() function
+-- ref: https://duckdb.org/docs/stable/guides/file_formats/csv_import
+INSERT INTO
+    staging.employees (department, employment_year)
+    SELECT * FROM read_csv('data/employees.csv');
+
+/* =============
+   CRUD - Read 
+   ============= */
+SELECT * FROM staging.employees; 
+--LIMIT 10;
+--OFFSET 10; 
+
+/* ==============
+   CRUD - Update 
+   ============== */
+-- modify existing data
+UPDATE staging.employees
+SET employment_year = 2023
+WHERE employee_id IN (98,99);
+--WHERE employee_id = 98 OR employee_id = 99; 
--- week2_crud_aggregate_hrdata/sql/ingest_employees.sql
+++
@@ -1,7 +0,0 @@
-CREAT SCHEMA IF NOT EXISTS STAGING;
-CREATE TABLE IF NOT EXISTS employees AS(
-    SELECT
-    * 
-    FROM 
-        read_csv_auto('data/employees.csv')
-);
